e08ef2ffca3fd7dcecf4f322c60e87e934f1ad7f
Anwar_Hossain <ranakrphone@gmail.com>
2026-07-07 00:23:47 +0600

    implemented JWT and Refresh Token flows

--- server/dist/utils/logger.js
+++
@@ -1,5 +0,0 @@
-export function logMessage(message, isError = false) {
-    const prefix = isError ? "[ERROR]" : "[INFO]";
-    const timestamp = new Date().toISOString();
-    console.log(`${prefix} [${timestamp}] - ${message}`);
-}
