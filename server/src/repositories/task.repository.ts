b779743e8cae62d0018c80a409f0ef80443133ec
Anwar_Hossain <ranakrphone@gmail.com>
2026-07-09 17:21:47 +0600

    task repository updated

--- server/src/repositories/task.repository.ts
+++ server/src/repositories/task.repository.ts
@@ -1,9 +1,7 @@
 import { prisma } from "../config/db.js";
-// প্রিজমার জেনারেটেড ফাইল থেকে Task টাইপ ইম্পোর্ট করলাম
 import { Task } from "../generated/client/client.js";
 
 export class TaskRepository {
-  // ১. একটি নতুন টাস্ক তৈরি করা
   static async create(title: string, userId: string): Promise<Task> {
     return prisma.task.create({
       data: {
@@ -13,7 +11,6 @@ export class TaskRepository {
     });
   }
 
-  // ২. ইউজারের আইডি অনুযায়ী সব টাস্ক খুঁজে বের করা
   static async findByUserId(userId: string): Promise<Task[]> {
     return prisma.task.findMany({
       where: {
@@ -22,11 +19,25 @@ export class TaskRepository {
     });
   }
 
-  // ৩. আইডি অনুযায়ী একটি একক টাস্ক খুঁজে বের করা (যা null হতে পারে)
   static async findById(id: string): Promise<Task | null> {
     return prisma.task.findUnique({
       where: {
         id,
+      },
+    });
+  }
+
+  // ৪. ডাটাবেজে স্ট্যাটাস আপডেট করার কোয়েরি মেথড
+  static async updateStatus(
+    id: string,
+    status: "TODO" | "IN_PROGRESS" | "DONE",
+  ): Promise<Task> {
+    return prisma.task.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
       },
     });
   }
