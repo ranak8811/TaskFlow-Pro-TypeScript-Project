ac5908fe89f712c5a8df816765c3b5b814ccbee2
Anwar_Hossain <ranakrphone@gmail.com>
2026-07-14 17:01:22 +0600

    react frontend setup done

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173, // লোকাল ডেভলপমেন্ট পোর্ট ফিক্সড করলাম
+  },
 });
